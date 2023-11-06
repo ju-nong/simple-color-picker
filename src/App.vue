0cd989ec8b1524e5ddd6b29b69c6d6f994fd86a8
이준용 <iiiuooyoo@gmail.com>
2023-11-06 11:28:00 +0900

    feat : 기능 구현 완료
    이제 스타일링만 남음

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
     <div class="container">
         <button @click="handleClick">Color Picker</button><br />
-        <button @click="handleCopy">{{ printColor }}</button>
+        <button @click="handleCopy(colorHex)">{{ colorHex }}</button><br />
+        <button @click="handleCopy(colorRGB)">{{ colorRGB }}</button><br />
 
         {{ supported ? "지원합니다" : "지원 안 합니다" }}
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onBeforeMount } from "vue";
+import { ref, computed, onBeforeMount } from "vue";
 
 // EyeDropper support check
 const supported = ref(false);
 const checkSupport = () => (supported.value = !!window.EyeDropper);
 
 const eyeDropper = ref(new window.EyeDropper());
-const printColor = ref("");
+
+const colorHex = ref("");
+const colorRGB = computed(() => {
+    const validHex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
+        colorHex.value,
+    );
+
+    return validHex
+        ? `rgb(${parseInt(validHex[1], 16)},
+            ${parseInt(validHex[2], 16)},
+            ${parseInt(validHex[3], 16)})`
+        : "";
+});
 
 async function handleClick() {
     const { sRGBHex } = await eyeDropper.value.open();
 
-    printColor.value = sRGBHex;
+    colorHex.value = sRGBHex;
 }
 
-function handleCopy() {}
+function handleCopy(color: string) {
+    try {
+        navigator.clipboard.writeText(color);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 onBeforeMount(() => {
     checkSupport();
