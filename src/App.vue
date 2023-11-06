4e6bf6796a464217aa46fedb63803e56bd1d38b6
이준용 <iiiuooyoo@gmail.com>
2023-11-06 11:04:12 +0900

    feat : EyeDropper로 sRGBHex 값 가져오기

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="container">
+        <button @click="handleClick">Color Picker</button><br />
+        <button @click="handleCopy">{{ printColor }}</button>
+
         {{ supported ? "지원합니다" : "지원 안 합니다" }}
     </div>
 </template>
@@ -7,9 +10,20 @@
 <script setup lang="ts">
 import { ref, onBeforeMount } from "vue";
 
+// EyeDropper support check
 const supported = ref(false);
-
 const checkSupport = () => (supported.value = !!window.EyeDropper);
+
+const eyeDropper = ref(new window.EyeDropper());
+const printColor = ref("");
+
+async function handleClick() {
+    const { sRGBHex } = await eyeDropper.value.open();
+
+    printColor.value = sRGBHex;
+}
+
+function handleCopy() {}
 
 onBeforeMount(() => {
     checkSupport();
